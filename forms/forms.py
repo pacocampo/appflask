d91af35ddec6b249ddfe08a9d6ea946507e1f207
pacocampo <paco.ocampor@gmail.com>
2016-10-04 21:29:19 -0500

    firebase added

--- forms/forms.py
+++ forms/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class Formulario(Form):
 	latitud = StringField('Latitud', validators=[DataRequired()])
 	longitud = StringField('Longitud', validators=[DataRequired()])
+
+class ProductoForm(Form):
+	nombre = StringField('Nombre de producto', validators=[DataRequired()])
+	precio = StringField('Precio', validators=[DataRequired()])
--- views/views.py
+++ views/views.py
@@ -4,12 +4,15 @@ de mi archivo'''
 from app import app
 from flask import render_template
 from miApi.api import Direccion
-from forms.forms import Formulario
+from forms.forms import Formulario, ProductoForm
+import json
+from firebase import firebase
 
 @app.route("/")
 def index():
 	contexto = ["Xime", "Elena", "Mariana"]
 	return render_template("index.html", data=contexto)
+
 
 @app.route("/api", methods=('GET', 'POST'))
 def api():
@@ -23,3 +26,14 @@ def api():
 			return render_template("404.html")
 	else:
 		return render_template("direccion.html", data=Direccion(), form=miForm)
+
+@app.route("/producto", methods=('GET', 'POST'))
+def producto():
+	formulario = ProductoForm()
+	fbase = firebase.FirebaseApplication('https://demoflask-99c26.firebaseio.com/', None)
+	if formulario.validate_on_submit():
+		fbase.post('/producto/categoria', {"categoria":formulario.nombre.data, "tag":formulario.precio.data})
+	result = fbase.get('/producto/categoria', None)
+	print(result)
+	return render_template("producto.html", data = result, form=formulario)
+
